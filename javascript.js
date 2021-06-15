b4c78cc41f39a89934c292bfc3ecd95dcc95b1f0
wilian-tavares <wilian.tavares@souunit.com.br>
2021-06-15 16:00:33 -0300

    solucionado BUG dos pontos repetitivos

--- javascript.js
+++ javascript.js
@@ -1,30 +1,66 @@
 var qtd = 0;
+var qtd_ponto = 0;
 
 function calcular(tipo, valor) {
     if (valor === 'C') {
         //limpar o visor
         document.getElementById('resultado').value = '';
+        qtd = 0;
+        qtd_ponto = 0;
     }
 
-    if (valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.') {
-        if (qtd > 0) {
+
+
+    else if (valor === '+' || valor === '-' || valor === '*' || valor === '/') {
+
+        if (qtd === 0) {
             document.getElementById('resultado').value += valor;
+            qtd += 1;
+            qtd_ponto = 0;
         }
 
-        document.getElementById('resultado').value += valor;
-        qtd += 1;
+        else if (qtd > 0) {
+            document.getElementById('resultado').value += "";
+        }
+    }
+
+
+
+    if (valor === ".") {
+
+        if (qtd_ponto === 0) {
+            document.getElementById('resultado').value += valor;
+            qtd_ponto += 1;
+        }
+        else if (qtd_ponto > 0 && valor === ".") {
+            document.getElementById('resultado').value += "";
+        }
+
 
     }
+
+
+
+
+
+
+
+
+
+
+
 
     if (valor === '=') {
         var valor_campo = eval(document.getElementById('resultado').value)
         document.getElementById('resultado').value = valor_campo;
         qtd = 0;
+        qtd_ponto = 0;
     }
 
     else if (tipo === 'valor') {
         document.getElementById('resultado').value += valor;
         qtd = 0;
+
     }
 }
 
@@ -41,7 +77,7 @@ function onSelectImage(event) {
     // assim que terminar de ler a imagem
     // jogá-la como plano de fundo da #calculadora
     reader.onload = function (event) {
-        document.querySelector('#calculadora').style.background = "url('" + event.target.result + "') no-repeat 0 0";
+        document.querySelector('#calculadora').style.background = "url('" + event.target.result + "') no-repeat 0 0 ";
     };
 
     // Informa ao reader para ler a image como base64
