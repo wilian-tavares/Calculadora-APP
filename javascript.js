4ab9f0212536ebd2baaed11357b6b345f75542ae
wilian-tavares <wilian.tavares@souunit.com.br>
2021-06-15 11:05:42 -0300

    Atualização.... mudança do Background pelo Usuario.

--- javascript.js
+++ javascript.js
@@ -28,3 +28,22 @@ function calcular(tipo, valor) {
     }
 }
 
+function onSelectImage(event) {
+    // se usuário não selecionou nenhum imagem não faça nada
+    if (!event.target.files.length) return;
+
+    // aqui pegaremos a primeira imagem da lista: FileList
+    var selectedFile = event.target.files[0];
+
+    // FileReader https://developer.mozilla.org/en-US/docs/Web/API/FileReader/readAsDataURL
+    var reader = new FileReader();
+
+    // assim que terminar de ler a imagem
+    // jogá-la como plano de fundo da #calculadora
+    reader.onload = function (event) {
+        document.querySelector('#calculadora').style.background = "url('" + event.target.result + "') no-repeat 0 0";
+    };
+
+    // Informa ao reader para ler a image como base64
+    reader.readAsDataURL(selectedFile);
+}
